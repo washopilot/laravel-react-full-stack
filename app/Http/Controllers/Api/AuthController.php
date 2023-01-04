af2d7480ca4699d5e64aa5c2ddd41c6832cd5da6
Fernando Chicaiza <washopilot@yahoo.com>
2023-01-04 18:44:06 -0500

    some advance code

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SignupRequest;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->validated();
+
+        if (Auth::attempt($credentials)) {
+            # code...
+        }
+    }
+
+    public function signup(SignupRequest $request)
+    {
+        $data = $request->validated();
+
+        /** @var \App\Models\User $user */
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password'])
+        ]);
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        // return response([
+        //     'user' => $user,
+        //     'token' => $token
+        // ]);
+
+        return response(compact('user', 'token'));
+    }
+
+    public function logout(Request $request)
+    {
+    }
+}
